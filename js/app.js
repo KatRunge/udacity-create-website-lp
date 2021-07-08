ac5e92c52b463c887a539b7ba37fbbea0c4638a2
Katja <katja.runge@jimdo.com>
2021-07-09 00:58:07 +0200

    feat: initial setu for styles

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const navList = document.getElementById("nav-list");
 const sections = document.querySelectorAll("section");
 
-
+//build navigation
 buildMenu = () => {
   for (section of sections) {
     const menuNames = section.id;
-
     navItem = document.createElement("li");
-    navItem.innerHTML = `<a href="#${menuNames}">${menuNames}</a>`;
+    section.classList = "section_style";
+    navItem.innerHTML = `<a href="#${menuNames}" class="navi_link">${menuNames.toUpperCase()}</a>`;
 
     navList.appendChild(navItem);
   }
