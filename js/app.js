f20c26ce249cc4c9472ae8fee3ea9321b8ba590b
Katja <katja.runge@jimdo.com>
2021-07-13 15:10:07 +0200

    feat: add smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,38 +1,15 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
 
-/**
- * Define Global Variables
- *
- */
+//  * Manipulating the DOM exercise.
+//  * Exercise programmatically builds navigation,
+//  * scrolls to anchors from navigation,
+//  * and highlights section in viewport upon scrolling.
 
+
+//global variables
 const navList = document.getElementById("nav-list");
 const sections = document.querySelectorAll("section");
 const buttonScroll = document.getElementById("button_scroll");
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
 
 // build the nav
 const buildMenu = () => {
@@ -41,14 +18,13 @@ const buildMenu = () => {
     section.classList = "section_style";
     navItem.innerHTML = `<a href="#${section.id}" class="navi_link">${section.id.toUpperCase()}</a>`;
     navList.appendChild(navItem);
-    console.log(navItem);
   }
 };
 
 buildMenu();
 
-// Add class 'active' to section when near top of viewport
 
+// Add class 'active' to section when near top of viewport
 const sectionActive = () => {
   sections.forEach((section) => {
     const elementViewport = section.getBoundingClientRect().top;
@@ -65,7 +41,6 @@ const sectionActive = () => {
 
 window.addEventListener("scroll", sectionActive);
 
-// Scroll to anchor ID using scrollTO event
 
 // Scroll to top button
 const topFunction = () => {
