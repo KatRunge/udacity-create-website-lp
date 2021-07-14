302183c261e27e85627de55013941e9ab234a82b
Katja <katja.runge@jimdo.com>
2021-07-14 21:30:22 +0200

    refactor: review all functions

--- js/app.js
+++ js/app.js
@@ -1,51 +1,59 @@
-
-//  * Manipulating the DOM exercise.
-//  * Exercise programmatically builds navigation,
-//  * scrolls to anchors from navigation,
-//  * and highlights section in viewport upon scrolling.
-
-
 //global variables
 const navList = document.getElementById("nav-list");
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll(".section_style");
 const buttonScroll = document.getElementById("button_scroll");
 
-
+// const buildMenu = () => {
+//   sections.map((section) => {
+//     const navItem = document.createElement("li");
+//     navItem.innerHTML = `<a class="navi_link">${section.id.toUpperCase()}</a>`;
+//     navList.appendChild(navItem);
+//   });
+// };
+// console.log(buildMenu);
 // build the navigation
-const buildMenu = () => {
-  for (section of sections) {
-    const navItem = document.createElement("li");
-    section.classList = "section_style";
-    // add anchor links to navigation based on the section id
-    navItem.innerHTML = `<a href="#${section.id}" class="navi_link">${section.id.toUpperCase()}</a>`;
-    navList.appendChild(navItem);
-  }
-};
+console.log(sections);
 
-buildMenu();
-
-
-// Add class 'active' to section when near top of viewport
-const sectionActiveClass = () => {
+const buildNavigation = () => {
   sections.forEach((section) => {
-    const viewportElement = section.getBoundingClientRect().top;
-    section.classList.remove("section_style_active");
-
-    const addActiveClass = (section) => {
-      if (viewportElement < 200 && viewportElement >= -150) {
-        section.classList.add("section_style_active");
-      }
-    };
-    return addActiveClass(section);
+    const navigationItem = document.createElement("li");
+    navigationItem.innerHTML = `<a class="nav_link">${section.id.toUpperCase()}</a>`;
+    navList.appendChild(navigationItem);
   });
 };
 
-window.addEventListener("scroll", sectionActiveClass);
+buildNavigation();
 
+// const buildMenu = () => {
+//   for (section of sections) {
+//     const navItem = document.createElement("li");
+//     navItem.innerHTML = `<a class="navi_link">${section.id.toUpperCase()}</a>`;
+//     navList.appendChild(navItem);
+//   }
+// };
 
-// Scroll to top button
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
+// buildMenu();
 
-buttonScroll.addEventListener("click", scrollToTop);
+// // Add class 'active' to section when near top of viewport
+// const sectionActiveClass = () => {
+//   sections.forEach((section) => {
+//     const viewportElement = section.getBoundingClientRect().top;
+//     section.classList.remove("section_style_active");
+
+//     const addActiveClass = (section) => {
+//       if (viewportElement < 200 && viewportElement >= -150) {
+//         section.classList.add("section_style_active");
+//       }
+//     };
+//     return addActiveClass(section);
+//   });
+// };
+
+// window.addEventListener("scroll", sectionActiveClass);
+
+// // Scroll to top button
+// const scrollToTop = () => {
+//   window.scrollTo({ top: 0, behavior: "smooth" });
+// };
+
+// buttonScroll.addEventListener("click", scrollToTop);
