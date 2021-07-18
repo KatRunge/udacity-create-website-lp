df277499e529c2170028feaa3ad63865366e7eb4
Katja <katja.runge@jimdo.com>
2021-07-18 14:39:08 +0200

    fix: update submition

--- js/app.js
+++ js/app.js
@@ -1,59 +1,62 @@
 //global variables
+//get element to add navigation list inside it
 const navList = document.getElementById("nav-list");
+
+//get all sections to use thteir id and add them to the navigatiton
 const sections = document.querySelectorAll(".section_style");
+
+//get button element to add scroll to Top function
 const buttonScroll = document.getElementById("button_scroll");
 
-// const buildMenu = () => {
-//   sections.map((section) => {
-//     const navItem = document.createElement("li");
-//     navItem.innerHTML = `<a class="navi_link">${section.id.toUpperCase()}</a>`;
-//     navList.appendChild(navItem);
-//   });
-// };
-// console.log(buildMenu);
-// build the navigation
-console.log(sections);
-
+//Build navigation
 const buildNavigation = () => {
   sections.forEach((section) => {
+    //create navitatiton item (li element)
     const navigationItem = document.createElement("li");
-    navigationItem.innerHTML = `<a class="nav_link">${section.id.toUpperCase()}</a>`;
+    //add navigation text inside li element
+    navigationItem.innerHTML = `<p >${section.id.toUpperCase()}</p>`;
+    //add a class to the li item, so some styles can be added
+    navigationItem.classList.add("nav_item");
+    //set an id attribute to li element too use it for the scroll event
+    navigationItem.setAttribute("id", `${section.id}`);
+    //add the li elements inside the ul list, the ul element was taken form the global variables
     navList.appendChild(navigationItem);
+    //add scroll event to each li element. Whenever you click an item it will scroll to the right section with the matching id name
+    navigationItem.addEventListener("click", () => {
+      //add behavior smooth so that the scroll event
+      section.scrollIntoView({ behavior: "smooth" });
+    });
   });
 };
 
 buildNavigation();
 
-// const buildMenu = () => {
-//   for (section of sections) {
-//     const navItem = document.createElement("li");
-//     navItem.innerHTML = `<a class="navi_link">${section.id.toUpperCase()}</a>`;
-//     navList.appendChild(navItem);
-//   }
-// };
 
-// buildMenu();
+//add active class to section when scrolling
+const sectionActiveClass = () => {
+  //for each section add an active class
+  sections.forEach((section) => {
+    //remove active class for every section when is not in viewport
+    section.classList.remove("section_style_active");
+    //Add condition for when a section is in viewporot
+    if (
+      //viewport must have this characteristics
+      section.getBoundingClientRect().top < 100 &&
+      section.getBoundingClientRect().top >= -150
+    ) {
+      //if the condition is fullfilled then the active class shall be added
+      section.classList.add("section_style_active");
+    }
+  });
+};
 
-// // Add class 'active' to section when near top of viewport
-// const sectionActiveClass = () => {
-//   sections.forEach((section) => {
-//     const viewportElement = section.getBoundingClientRect().top;
-//     section.classList.remove("section_style_active");
+//Add scroll event listener
+window.addEventListener("scroll", sectionActiveClass);
 
-//     const addActiveClass = (section) => {
-//       if (viewportElement < 200 && viewportElement >= -150) {
-//         section.classList.add("section_style_active");
-//       }
-//     };
-//     return addActiveClass(section);
-//   });
-// };
-
-// window.addEventListener("scroll", sectionActiveClass);
-
-// // Scroll to top button
-// const scrollToTop = () => {
-//   window.scrollTo({ top: 0, behavior: "smooth" });
-// };
-
-// buttonScroll.addEventListener("click", scrollToTop);
+// Scroll to top button
+const buttonScrollTop = () => {
+  //scroll event that ads a smooth behavior to the scroll action, goes directly to the top part of the page
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+//add event to button, whenever you click on it, it will directly scroll to the top page
+buttonScroll.addEventListener("click", buttonScrollTop);
